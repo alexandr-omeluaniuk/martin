89c94e9c5754baa90c60758c4bbc98a29303fd39
Alexandr Omeluaniuk <alexandr.omeluaniuk@wisent-media.by>
2020-12-12 16:17:19 +0300

    Modules.

--- src/main/resources/static/martin-platform-client/src/module/core/module.js
+++ src/main/resources/static/martin-platform-client/src/module/core/module.js
@@ -5,7 +5,6 @@
  */
 
 import Subscriptions from './view/Subscriptions';
-import Users from './view/Users';
 import { Module } from '../../util/model/Module';
 
 export const core = new Module('core', '/core', [{
@@ -13,9 +12,4 @@ export const core = new Module('core', '/core', [{
         path: '/subscriptions',
         icon: 'api',
         component: Subscriptions
-    }, {
-        id: 'users',
-        path: '/users',
-        icon: 'group',
-        component: Users
     }]);
