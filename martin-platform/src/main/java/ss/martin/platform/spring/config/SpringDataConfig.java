002a0a181c5183a09536c3e25a0903fa0be4c84c
Alexandr Omeluaniuk <alexandr.omeluaniuk@wisent-media.by>
2021-03-10 20:48:39 +0300

    Refactoring.

--- martin-platform/src/main/java/ss/martin/platform/spring/config/SpringDataConfig.java
+++ martin-platform/src/main/java/ss/martin/platform/spring/config/SpringDataConfig.java
@@ -16,14 +16,12 @@
  */
 package ss.martin.platform.spring.config;
 
-import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import ss.entity.martin.SystemUser;
 
 /**
@@ -32,26 +30,13 @@ import ss.entity.martin.SystemUser;
  */
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EntityScan("ss.entity")
 public class SpringDataConfig {
     /** Auditor aware. */
     @Autowired
     private AuditorAware<SystemUser> auditorAware;
-    /** Data source. */
-    @Autowired
-    private DataSource dataSource;
     @Bean
     public AuditorAware<SystemUser> auditorAware() {
         return auditorAware;
-    }
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource);
-        em.setPackagesToScan("ss.entity");
-        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-//        if (additionalProperties() != null) {
-//            em.setJpaProperties(additionalProperties());
-//        }
-        return em;
     }
 }
